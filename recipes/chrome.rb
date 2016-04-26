7ed20d14066b349aacd88760aafd98e891975c18
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-04-25 21:42:56 -0700

    Moved Chrome item to correct recipe

--- recipes/chrome.rb
+++ recipes/chrome.rb
@@ -3,3 +3,14 @@
 # Recipe:: chrome
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+apt_repository "chrome" do
+    uri "http://dl.google.com/linux/chrome/deb/"
+    distribution "stable"
+    components ["main"]
+    arch "amd64"
+    key "https://dl-ssl.google.com/linux/linux_signing_key.pub"
+    action :add
+end
+
+package "google-chrome-stable"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,14 +6,4 @@
 
 include_recipe 'apt::default'
 include_recipe 'nodejs::default'
-
-apt_repository "chrome" do
-    uri "http://dl.google.com/linux/chrome/deb/"
-    distribution "stable"
-    components ["main"]
-    arch "amd64"
-    key "https://dl-ssl.google.com/linux/linux_signing_key.pub"
-    action :add
-end
-
-package "google-chrome-stable"
+include_recipe 'developer-base::chrome'
