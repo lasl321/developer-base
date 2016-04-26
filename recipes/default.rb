4d6203857eb18a661a1ce1e45f4ee9450b2d4bf9
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-04-25 21:48:16 -0700

    Added utilities

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,6 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'apt::default'
+include_recipe 'developer-base::utilities'
 include_recipe 'nodejs::default'
 include_recipe 'developer-base::chrome'
---
+++ recipes/utilities.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: developer-base
+# Recipe:: utilities
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'curl' do
+  action :install
+end
+
+package 'tree' do
+  action :install
+end
