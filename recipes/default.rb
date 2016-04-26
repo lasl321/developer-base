a0a7818a649c49bc9bdbb84065a2a014b247b8f3
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-04-25 21:39:55 -0700

    Added item to install Google Chrome

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,14 @@
 
 include_recipe 'apt::default'
 include_recipe 'nodejs::default'
+
+apt_repository "chrome" do
+    uri "http://dl.google.com/linux/chrome/deb/"
+    distribution "stable"
+    components ["main"]
+    arch "amd64"
+    key "https://dl-ssl.google.com/linux/linux_signing_key.pub"
+    action :add
+end
+
+package "google-chrome-stable"
